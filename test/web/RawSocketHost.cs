8405e0a77e6df697b3fac8351651a1cfd7894fbe
Anton Firszov <antonfir@gmail.com>
2020-04-02 02:18:50 +0200

    refactor hosting logic to KestrelHost.cs

---
+++ test/web/RawSocketHost.cs
@@ -0,0 +1,19 @@
+namespace web
+{
+    public class RawSocketHost
+    {
+        private string[] _args;
+        private readonly CommandLineOptions _options;
+
+        public RawSocketHost(CommandLineOptions options, string[] args)
+        {
+            _options = options;
+            _args = args;
+        }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
